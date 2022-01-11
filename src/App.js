898e26b1fa34289f7b63f45ce7b20b5e2e922e89
Emilio Feliciano <feliciano.emilioj@live.com>
2022-01-11 12:36:14 -0800

    feat: :sparkles: Added buttons and change colors.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Net from 'vanta/dist/vanta.net.min'
 import HeadComponent from './components/HeadComponent/HeadComponent'
+import { ArrowLeft, ArrowRight } from '@icon-park/react'
 import './App.css'
 
 const App = () => {
+  const [page] = useState(0)
   const [vantaEffect, setVantaEffect] = useState(0)
 
   const myRef = useRef(null)
+  // gold metallic color: 0xd4af37,
   useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(
@@ -19,7 +22,8 @@ const App = () => {
           minWidth: 200.0,
           scale: 1.0,
           scaleMobile: 1.0,
-          color: 0xcdcdcd,
+          color: 0x9b8248,
+          backgroundColor: 0x141414,
           points: 14.0,
           maxDistance: 21.0,
           spacing: 16.0,
@@ -34,6 +38,18 @@ const App = () => {
   return (
     <div ref={myRef}>
       <HeadComponent />
+      <div className="arrow-btns-container">
+        <div className="arrow-btn-wrapper">
+          <button hidden={(page===2)} className="btn arrow-btn">
+            <ArrowLeft theme="outline" size="32" fill="#fff" strokeWidth={3} />
+          </button>
+        </div>
+        <div className="arrow-btn-wrapper">
+          <button className="btn arrow-btn">
+            <ArrowRight theme="outline" size="32" fill="#fff" strokeWidth={3} />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/HeadComponent/HeadComponent.js
+++ src/components/HeadComponent/HeadComponent.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import './HeadComponent.css'
+import { Slide } from 'react-awesome-reveal'
+import { FileCode, ExperimentOne, FlightAirflow } from '@icon-park/react'
 
 const HeadComponent = () => {
   return (
-    <div id='alias-content'>
-      <h1 className='text'>Hi,</h1>
-      <h2 className='text'>my name is Emilio a.k.a FelicianoEJ</h2>
-    </div>
+    <Slide>
+      <div id="alias-content">
+        <h1 className="text">Hi,</h1>
+        <h2 className="text">I am Emilio a.k.a FelicianoEJ</h2>
+        <br />
+        <h2 className="text">
+          <FileCode theme="outline" size="20" fill="#fff" strokeWidth={3} />
+          &nbsp; Software Engineer
+        </h2>
+        <h2 className="text">
+          <ExperimentOne
+            theme="outline"
+            size="20"
+            fill="#fff"
+            strokeWidth={3}
+          />
+          &nbsp; Test Lead
+        </h2>
+        <h2 className="text">
+          <FlightAirflow
+            theme="outline"
+            size="20"
+            fill="#fff"
+            strokeWidth={3}
+          />
+          &nbsp; Pilot
+        </h2>
+        <br />
+      </div>
+    </Slide>
   )
 }
 
