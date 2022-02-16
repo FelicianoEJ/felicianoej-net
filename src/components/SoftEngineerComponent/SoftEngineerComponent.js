d558f4fb76eb847c263b5c05d4fb3a29c388d1bb
Emilio Feliciano <feliciano.emilioj@live.com>
2022-02-16 15:38:30 -0800

    fix: :bug: Change wrong reference to linkedin profile

--- src/components/SoftEngineerComponent/SoftEngineerComponent.js
+++ src/components/SoftEngineerComponent/SoftEngineerComponent.js
@@ -3,7 +3,8 @@ import './SoftEngineerComponent.css'
 import { Slide } from 'react-awesome-reveal'
 
 const SoftEngineerComponent = () => {
-  const linkedinRef = 'https://www.christophergironofficial.com/'
+  const linkedinRef = 'https://www.linkedin.com/in/emilio-feliciano-330a1021b/'
+
   return (
     <Slide>
       <div className="content">
@@ -12,16 +13,13 @@ const SoftEngineerComponent = () => {
           Passionate software engineer with experience in multiple areas such as
           web development, network engineering, and internal corporate
           applications. Experienced in working with monolithic systems and
-          microservices utilizing software-defined networking approaches. Highly
-          focus on developing technical experience by participating in
-          cooperative and professional environments while providing a very
-          comprehensive intellectual service.
+          microservices utilizing software-defined networking approaches.
         </h2>
         <br />
       </div>
       <br />
       <div className="content">
-        <a href={linkedinRef}>LinkedIn</a>
+        <a href={linkedinRef}><img alt="LinkedIn" src="https://content.linkedin.com/content/dam/me/brand/en-us/brand-home/logos/In-Blue-Logo.png.original.png" width="32" /></a>
       </div>
       <br />
     </Slide>
